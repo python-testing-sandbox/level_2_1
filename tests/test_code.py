96fb2857b9af3dc9a2f520880281b14e5771ea26
fattybobcat <alexander@zipsale.co.uk>
2021-07-06 19:36:47 +0300

    add test1, rename code->codes, fix yml

---
+++ tests/test_code.py
@@ -0,0 +1,19 @@
+from unittest import mock
+import pytest
+
+
+from codes import (load_obscene_words)
+
+
+@pytest.mark.parametrize(
+    'db_path, expected',
+    [
+        ([[1, 2, 3], [4]], {1, 2, 3, 4}),
+        ([[1, 2], ['x', 'y']], {1, 2, 'x', 'y'}),
+        ([], set()),
+    ]
+)
+def test_load_obscene_words(db_path, expected, mocker):
+    mock_sqlite = mocker.patch('codes.sqlite3')
+    mock_sqlite.connect().cursor().execute().fetchall.return_value = db_path
+    assert load_obscene_words('path') == expected
