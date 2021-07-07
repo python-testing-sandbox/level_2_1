c60d31ad1e2577c231dade74fb167011ae4efb1a
fattybobcat <alexander@zipsale.co.uk>
2021-07-07 21:21:41 +0300

    add 1 test

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,6 +1,7 @@
 import pytest
 
-from codes import (load_obscene_words, fetch_detailed_pull_requests, get_all_filepathes_recursively)
+from codes import (load_obscene_words, fetch_detailed_pull_requests, get_all_filepathes_recursively,
+                   get_params_from_config, )
 
 
 @pytest.mark.parametrize(
@@ -32,7 +33,7 @@ def test_fetch_detailed_pull_requests(mocker, open_pull_requests, pull_request, 
 
 
 @pytest.mark.parametrize(
-    'path_file, is_dir, extension,expected',
+    'path_file, is_dir, extension, expected',
     [
         (['test_code.py, confest.py'], True, 'py', []),
         (['test_code.py, confest.py'], False, 'py', ['test_code.py, confest.py']),
@@ -42,3 +43,21 @@ def test_get_all_filepathes_recursively(path_file, is_dir, extension, expected, 
     mocker.patch('codes.Path.glob', return_value=path_file)
     mocker.patch('codes.os.path.isdir', return_value=is_dir)
     assert get_all_filepathes_recursively('tests', extension) == expected
+
+
+@pytest.mark.parametrize(
+    'config_section, params, expected',
+    [
+        (False, {'processes': '0'}, {}),
+        (True, {'processes': '0'}, {'processes': 0}),
+        (False, {'processes': '0', 'exit_zero': 'True'}, {}),
+        (True, {'reorder_vocabulary': 'True', 'exit_zero': 'True'}, {'reorder_vocabulary': True, 'exit_zero': True}),
+        (True, {'verbosity': '-20', 'process_dots': 'False'}, {'process_dots': False, 'verbosity': -20}),
+    ]
+ )
+def test_get_params_from_config(mocker, config_section, params, expected):
+    mocker_config_parser = mocker.patch('codes.configparser', autospec=True)
+    mocker_config_parser.ConfigParser().read.return_value = None
+    mocker_config_parser.ConfigParser().has_section.return_value = config_section
+    mocker_config_parser.ConfigParser().__getitem__.return_value = params
+    assert get_params_from_config('config_path') == expected
