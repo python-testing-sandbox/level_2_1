47a507878249f4bd08a0573167a9d5e3e30a4a33
fattybobcat <alexander@zipsale.co.uk>
2021-07-07 19:01:44 +0300

    add test

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,7 +1,8 @@
 import pytest
 
-
-from codes import (load_obscene_words)
+from io import StringIO
+import sys
+from codes import (load_obscene_words, fetch_detailed_pull_requests)
 
 
 @pytest.mark.parametrize(
@@ -16,3 +17,17 @@ def test_load_obscene_words(db_path, expected, mocker):
     mock_sqlite = mocker.patch('codes.sqlite3')
     mock_sqlite.connect().cursor().execute().fetchall.return_value = db_path
     assert load_obscene_words('path') == expected
+
+
+@pytest.mark.parametrize(
+    'open_pull_requests, pull_request, expected',
+    [
+        ([{'number': 0}, {'number': 1}], {'number': 1}, {1: {'number': 1}}),
+        ([{'number': 2}], None, {}),
+        ([{'number': 3}], {'number': 4}, {4: {'number': 4}}),
+     ]
+ )
+def test_fetch_detailed_pull_requests(mocker, open_pull_requests, pull_request, expected):
+    mock_api = mocker.Mock()
+    mock_api.fetch_pull_request.return_value = pull_request
+    assert fetch_detailed_pull_requests(mock_api, open_pull_requests) == expected
