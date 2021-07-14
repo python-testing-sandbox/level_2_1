91712cd232af951cc6ab3a1908169fb0968e369e
fattybobcat <alexander@zipsale.co.uk>
2021-07-14 09:17:51 +0300

    add test _load_workbook_from_xls

--- tests/test_code.py
+++ tests/test_code.py
@@ -5,7 +5,8 @@ from pytz import timezone
 from PIL import UnidentifiedImageError
 from codes import (load_obscene_words, fetch_detailed_pull_requests, get_all_filepathes_recursively,
                    get_params_from_config, _set_listed_at, DateTimeProcessor, fetch_badges_urls,
-                   skip_exceptions_to_reraise, get_content_from_file, ColumnError,)
+                   skip_exceptions_to_reraise, get_content_from_file, ColumnError,
+                   _load_workbook_from_xls,)
 from contextlib import nullcontext
 
 
@@ -200,5 +201,20 @@ def test_get_content_from_file(mocker, guess_encoding, data, error, expected):
     assert get_content_from_file('text.txt', guess_encoding) == expected
 
 
-def test_load_workbook_from_xls():
-    pass
+
+@pytest.mark.parametrize(
+    'value, ctype',
+    [
+        (1, 2),
+        (1, 1),
+    ]
+)
+def test_load_workbook_from_xls(mocker, value, ctype):
+    xls_workbook_mock = mocker.patch('xlrd.open_workbook', autospec=True)
+    workbook_mock = mocker.patch('codes.Workbook', return_value=mocker.MagicMock())
+    xls_sheet_mock = xls_workbook_mock().sheet_by_index()
+    xls_sheet_mock.cell().value = value
+    xls_sheet_mock.cell().ctype = ctype
+    xls_workbook_mock().datemode = 1
+    mocker.patch('datetime.datetime', return_value=1)
+    assert _load_workbook_from_xls('super_path', 'super_content') == workbook_mock()
